c6daf20a7542e3d9b61cedd91c0e2899f73e1d49
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 18:39:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 // script.js
 $(document).ready(function() {
-  $(window).scroll(function() {
-    $(".card").each(function() {
-      if ($(this).visible(true)) {
-        $(this).addClass("animate");
+  $.getJSON("timeline.json", function(data) {
+    var timelineEvents = data.events;
+    var $timelineColumn = $("#timeline-column");
+    var $timelineNav = $("#timeline-nav");
+
+    $.each(timelineEvents, function(index, event) {
+      var isOdd = index % 2 === 0;
+      var $card = $("<div>").addClass("card").addClass(isOdd ? "odd" : "even");
+      var $cardBorder = $("<div>").addClass("card-border");
+      var $cardLineContainer = $("<div>").addClass("card-line-container");
+      var $cardLine = $("<div>").addClass("card-line");
+      var $cardLineCircle = $("<div>").addClass("card-line-circle");
+
+      var $cardDate = $("<div>").addClass("card-date").text(event.date);
+      var $cardText = $("<div>").addClass("card-text");
+      var $cardTextTitle = $("<div>").addClass("card-text-title").text(event.title);
+      var $cardTextBody = $("<div>").addClass("card-text-body").html(event.description);
+
+      $cardText.append($cardTextTitle, $cardTextBody);
+      $cardBorder.append($cardDate, $cardText);
+      $cardLineContainer.append($cardLine, $cardLineCircle);
+      $card.append($cardBorder, $cardLineContainer);
+      $timelineColumn.append($card);
+
+      var $timelineNavItem = $("<span>")
+        .addClass("timeline-nav-item")
+        .text(event.date)
+        .attr("data-id", event.id);
+      $timelineNav.append($timelineNavItem);
+    });
+
+    $(window).scroll(function() {
+      $(".card").each(function() {
+        if ($(this).visible(true)) {
+          $(this).addClass("animate");
+        }
+      });
+    });
+
+    $(".timeline-nav-item").on("click", function() {
+      var targetId = $(this).attr("data-id");
+      var targetElement = $("#" + targetId);
+
+      if (targetElement.length > 0) {
+        $("html, body").animate({
+          scrollTop: targetElement.offset().top
+        }, 800);
       }
     });
   });
