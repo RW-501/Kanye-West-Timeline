9093fd9f00badf0f78d2178c7f968456c2a354d8
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 20:12:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,63 @@
 $(document).ready(function() {
-  // Load timeline data from JSON file
-  $.getJSON('timeline.json', function(data) {
+  // Load event data from JSON file
+  $.getJSON("timeline.json", function(data) {
     var events = data.events;
-    var timelineNav = $('#timeline-nav');
-    var timelineColumn = $('#timeline-column');
+    var timelineNav = $("#timeline-nav");
+    var timelineColumn = $("#timeline-column");
 
     // Generate timeline navigation
-    for (var i = 0; i < events.length; i++) {
-      var event = events[i];
-      var timelineNavItem = $('<a>', {
-        href: '#' + event.id,
-        text: event.date,
-        class: 'timeline-nav-item'
-      });
-      timelineNav.append(timelineNavItem);
-    }
+    events.forEach(function(event) {
+      timelineNav.append('<a href="#' + event.id + '">' + event.date + '</a>');
+    });
 
     // Generate timeline cards
-    for (var i = 0; i < events.length; i++) {
-      var event = events[i];
-      var card = $('<div>', {
-        id: event.id,
-        class: 'card ' + (i % 2 === 0 ? 'even' : 'odd')
-      });
-
-      var cardContent = $('<div>', { class: 'card-content' });
-
-      var cardDate = $('<div>', { class: 'card-date', text: event.date });
-      var cardTitle = $('<div>', { class: 'card-text-title', text: event.title });
-
-      cardContent.append(cardDate);
-      cardContent.append(cardTitle);
-      card.append(cardContent);
-
-      // Add click event to card to show event details
-      card.click(function() {
-        var eventId = $(this).attr('id');
-        var clickedEvent = events.find(function(event) {
-          return event.id === eventId;
-        });
-        showEventDetails(clickedEvent);
-      });
-
+    events.forEach(function(event, index) {
+      var card = $('<div class="card"></div>');
+      card.addClass(index % 2 === 0 ? "left" : "right");
+      card.attr("id", event.id);
+      card.append('<div class="card-date">' + event.date + '</div>');
+      card.append('<div class="card-text"><div class="card-text-title">' + event.title + '</div></div>');
       timelineColumn.append(card);
-    }
-  });
 
-  // Show event details
-  function showEventDetails(event) {
-    var eventDetails = $('#eventDetails');
-    var eventDetailsContent = $('#eventDetailsContent');
-
-    // Populate event details content
-    eventDetailsContent.html('');
-    var eventDetailsTitle = $('<h2>', { text: event.title });
-    var eventDetailsDescription = $('<div>', { html: event.longDescription });
-    var eventDetailsHashtags = $('<div>', { text: 'Hashtags: ' + event.hashtags.join(', ') });
-    eventDetailsContent.append(eventDetailsTitle);
-    eventDetailsContent.append(eventDetailsDescription);
-    eventDetailsContent.append(eventDetailsHashtags);
-
-    // Show event details
-    eventDetails.removeClass('hidden');
-
-    // Hide event details when close button is clicked
-    $('#closeButton').click(function() {
-      eventDetails.addClass('hidden');
-    });
-  }
-
-  // Search events by hashtag
-  $('#searchInput').keyup(function() {
-    var searchValue = $(this).val().toLowerCase();
-    $('.card').each(function() {
-      var cardHashtags = $(this).find('.card-text-body').data('hashtags');
-      var cardMatchesSearch = cardHashtags.some(function(hashtag) {
-        return hashtag.toLowerCase().includes(searchValue);
+      card.click(function() {
+        showEventDetails(event);
       });
-      $(this).toggle(cardMatchesSearch);
     });
+
+    // Handle search functionality
+    $("#search-input").on("input", function() {
+      var searchTerm = $(this).val().toLowerCase();
+      $(".card").each(function() {
+        var cardText = $(this).find(".card-text").text().toLowerCase();
+        if (cardText.includes(searchTerm)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+
+    // Handle navigation scrolling
+    timelineNav.find("a").click(function(e) {
+      e.preventDefault();
+      var targetId = $(this).attr("href");
+      var targetOffset = $(targetId).offset().top;
+      $("html, body").animate({ scrollTop: targetOffset }, 500);
+    });
+
+    // Event details modal
+    var modal = $("#event-details-modal");
+    var modalContent = $(".modal-content");
+    var closeModal = $(".close");
+
+    closeModal.click(function() {
+      modalContent.removeClass("show");
+    });
+
+    function showEventDetails(event) {
+      modal.find(".event-details-title").text(event.title);
+      modal.find(".event-details-description").html(event.longDescription);
+      modalContent.addClass("show");
+    }
   });
 });
