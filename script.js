bbd5fd4e8e6c6398ecee5e2c89502d0d8618e50a
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 19:50:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,83 @@
-// script.js
 $(document).ready(function() {
-  $.getJSON("timeline.json", function(data) {
-    var timelineEvents = data.events;
-    var $timelineColumn = $("#timeline-column");
-    var $timelineNav = $("#timeline-nav");
+  // Load timeline data from JSON file
+  $.getJSON('timeline.json', function(data) {
+    var events = data.events;
+    var timelineNav = $('#timeline-nav');
+    var timelineColumn = $('#timeline-column');
 
-    $.each(timelineEvents, function(index, event) {
-      var isOdd = index % 2 === 0;
-      var $card = $("<div>").addClass("card").addClass(isOdd ? "odd" : "even");
-      var $cardBorder = $("<div>").addClass("card-border");
-      var $cardLineContainer = $("<div>").addClass("card-line-container");
-      var $cardLine = $("<div>").addClass("card-line");
-      var $cardLineCircle = $("<div>").addClass("card-line-circle");
-
-      var $cardDate = $("<div>").addClass("card-date").text(event.date);
-      var $cardText = $("<div>").addClass("card-text");
-      var $cardTextTitle = $("<div>").addClass("card-text-title").text(event.title);
-      var $cardTextBody = $("<div>").addClass("card-text-body").html(event.description);
-
-      $cardText.append($cardTextTitle, $cardTextBody);
-      $cardBorder.append($cardDate, $cardText);
-      $cardLineContainer.append($cardLine, $cardLineCircle);
-      $card.append($cardBorder, $cardLineContainer);
-      $timelineColumn.append($card);
-
-      var $timelineNavItem = $("<span>")
-        .addClass("timeline-nav-item")
-        .text(event.date)
-        .attr("data-id", event.id);
-      $timelineNav.append($timelineNavItem);
-    });
-
-    $(window).scroll(function() {
-      $(".card").each(function() {
-        if ($(this).visible(true)) {
-          $(this).addClass("animate");
-        }
+    // Generate timeline navigation
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      var timelineNavItem = $('<a>', {
+        href: '#' + event.id,
+        text: event.date,
+        class: 'timeline-nav-item'
       });
+      timelineNav.append(timelineNavItem);
+    }
+
+    // Generate timeline cards
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      var card = $('<div>', {
+        id: event.id,
+        class: 'card ' + (i % 2 === 0 ? 'even' : 'odd')
+      });
+
+      var cardContent = $('<div>', { class: 'card-content' });
+
+      var cardDate = $('<div>', { class: 'card-date', text: event.date });
+      var cardTitle = $('<div>', { class: 'card-text-title', text: event.title });
+
+      cardContent.append(cardDate);
+      cardContent.append(cardTitle);
+      card.append(cardContent);
+
+      // Add click event to card to show event details
+      card.click(function() {
+        var eventId = $(this).attr('id');
+        var clickedEvent = events.find(function(event) {
+          return event.id === eventId;
+        });
+        showEventDetails(clickedEvent);
+      });
+
+      timelineColumn.append(card);
+    }
+  });
+
+  // Show event details
+  function showEventDetails(event) {
+    var eventDetails = $('#eventDetails');
+    var eventDetailsContent = $('#eventDetailsContent');
+
+    // Populate event details content
+    eventDetailsContent.html('');
+    var eventDetailsTitle = $('<h2>', { text: event.title });
+    var eventDetailsDescription = $('<div>', { html: event.longDescription });
+    var eventDetailsHashtags = $('<div>', { text: 'Hashtags: ' + event.hashtags.join(', ') });
+    eventDetailsContent.append(eventDetailsTitle);
+    eventDetailsContent.append(eventDetailsDescription);
+    eventDetailsContent.append(eventDetailsHashtags);
+
+    // Show event details
+    eventDetails.removeClass('hidden');
+
+    // Hide event details when close button is clicked
+    $('#closeButton').click(function() {
+      eventDetails.addClass('hidden');
     });
+  }
 
-    $(".timeline-nav-item").on("click", function() {
-      var targetId = $(this).attr("data-id");
-      var targetElement = $("#" + targetId);
-
-      if (targetElement.length > 0) {
-        $("html, body").animate({
-          scrollTop: targetElement.offset().top
-        }, 800);
-      }
+  // Search events by hashtag
+  $('#searchInput').keyup(function() {
+    var searchValue = $(this).val().toLowerCase();
+    $('.card').each(function() {
+      var cardHashtags = $(this).find('.card-text-body').data('hashtags');
+      var cardMatchesSearch = cardHashtags.some(function(hashtag) {
+        return hashtag.toLowerCase().includes(searchValue);
+      });
+      $(this).toggle(cardMatchesSearch);
     });
   });
 });
-
-$.fn.visible = function(partial) {
-  var $t = $(this),
-    $w = $(window),
-    viewTop = $w.scrollTop(),
-    viewBottom = viewTop + $w.height(),
-    _top = $t.offset().top,
-    _bottom = _top + $t.height(),
-    compareTop = partial === true ? _bottom : _top,
-    compareBottom = partial === true ? _top : _bottom;
-
-  return (
-    compareBottom <= viewBottom &&
-    compareTop >= viewTop
-  );
-};
